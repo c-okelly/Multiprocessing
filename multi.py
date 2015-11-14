4b3af2fe10d714235c45e7141cf5b9040e26958d
Conor O'Kelly <okellyconor@gmail.com>
2015-11-14 19:29:45 +0000

    Multi.py working correctly. Only write up needed.

--- multi.py
+++ multi.py
@@ -7,13 +7,14 @@ Have second function that will run prime calculator on a number of cores dependi
 
 """
 
+import time
 from multiprocessing import Process
 
 
 # Prime calculator
 
 def prime_calculator(start,finish):
-    print ("Start of calculator starting at ", start)
+    print ("Start of calculator starting %i to %i " % (start, finish))
     list_of_primes = []
 
     for number in range(start,finish+1):
@@ -30,18 +31,72 @@ def prime_calculator(start,finish):
             # print (number, "is a prime number")
             list_of_primes.append(number)
 
-    print(list_of_primes)
+    #print(list_of_primes)
     return list_of_primes
 
+def calculator_1_process():
 
+     a = Process(target=prime_calculator, args=(1,10000))
+
+     start_time = time.time()
+     a.start()
+     a_boolen = a.is_alive()
+
+     while a_boolen == True:
+         a_boolen = a.is_alive()
+
+     print("--- 1 process took %s seconds --- \n" % (time.time() - start_time))
+
+def calculator_2_process():
+
+    a = Process(target=prime_calculator, args=(1,5000))
+    b = Process(target=prime_calculator, args=(5000,10000))
+
+    start_time = time.time()
+    a.start()
+    b.start()
+    a_boolen, b_boolen = a.is_alive(), b.is_alive()
+
+    while a_boolen == True or b_boolen == True:
+         a_boolen, b_boolen = a.is_alive(), b.is_alive()
+
+    print("--- 2 process took %s seconds --- \n" % (time.time() - start_time))
+
+def calculator_4_process():
+
+    a = Process(target=prime_calculator, args=(1,2500))
+    b = Process(target=prime_calculator, args=(2500,5000))
+    c = Process(target=prime_calculator, args=(5000,7500))
+    d = Process(target=prime_calculator, args=(7500,10000))
+
+
+    start_time = time.time()
+    a.start()
+    b.start()
+    c.start()
+    d.start()
+
+    a_boolen, b_boolen, c_boolen, d_boolen = a.is_alive(), b.is_alive(), c.is_alive(), d.is_alive()
+
+    while a_boolen == True or b_boolen == True or c_boolen == True or d_boolen == True:
+         a_boolen, b_boolen, c_boolen, d_boolen = a.is_alive(), b.is_alive(), c.is_alive(), d.is_alive()
+
+    print("--- 4 process took %s seconds --- \n" % (time.time() - start_time))
+
+
+def run_all_calcultators():
+
+    print ("""This program will run a prime calculator in four different ways
+    First by using just one process. Then buy using two and then 4.
+    Each process run time will be measured adn printed out.
+    The process will also be running an extra process used to monitor
+    (using a while loop) if the process has completed.
+    """)
+
+    calculator_1_process()
+    calculator_2_process()
+    calculator_4_process()
 
 if __name__ == '__main__':
 
-    jobs = []
-
-    a = Process(target=prime_calculator, args=(1,10000))
-    b = Process(target=prime_calculator, args=(10000,20000))
-
-    a.start()
-    b.start()
-
+    run_all_calcultators()
